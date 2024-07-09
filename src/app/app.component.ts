5d3d5030c7b7569530585cbb8ea0ac2ede5b85ed
Ignacio Lopez <ignacio.lopez@tenpo.cl>
2024-07-09 19:43:39 -0300

    4 columns view and github workflow to update byli-cms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { List } from 'immutable';
-import { Observable } from 'rxjs';
-import { MenuService } from 'src/service/menu.service';
+import {Component} from '@angular/core';
+import {Router} from '@angular/router';
+import {List} from 'immutable';
+import {Observable} from 'rxjs';
+import {MenuService} from 'src/service/menu.service';
+import {OverlayContainer} from "@angular/cdk/overlay";
 
 @Component({
   selector: 'app-root',
@@ -13,7 +14,7 @@ export class AppComponent {
   menu: Observable<List<any>>;
   router: Router;
 
-  constructor(menuService: MenuService, router: Router) {
+  constructor(menuService: MenuService, router: Router,private overlayContainer: OverlayContainer) {
     this.menu = menuService.getMenu();
     this.router = router;
   }
