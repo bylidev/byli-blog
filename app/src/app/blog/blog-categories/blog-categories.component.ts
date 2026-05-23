fd425579653b1154f6531551f6a0daf9da29d23e
Ignacio Lopez <you@example.com>
2026-05-23 12:19:24 -0300

    updates

--- app/src/app/blog/blog-categories/blog-categories.component.ts
+++ app/src/app/blog/blog-categories/blog-categories.component.ts
@@ -1,22 +1,17 @@
-import {Component} from '@angular/core';
-import {Observable} from "rxjs";
-import {List} from "immutable";
-import {AsyncPipe, NgClass, NgForOf} from "@angular/common";
-import {Router} from "@angular/router";
-import {MenuService} from "../../../service/menu.service";
-import {MatChip} from "@angular/material/chips";
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { List } from 'immutable';
+import { AsyncPipe, NgClass, NgForOf } from '@angular/common';
+import { Router } from '@angular/router';
+import { MenuService } from '../../../service/menu.service';
+import { MatChip } from '@angular/material/chips';
 
 @Component({
   selector: 'app-blog-categories',
   standalone: true,
-  imports: [
-    AsyncPipe,
-    MatChip,
-    NgClass,
-    NgForOf
-  ],
+  imports: [AsyncPipe, MatChip, NgClass, NgForOf],
   templateUrl: './blog-categories.component.html',
-  styleUrl: './blog-categories.component.scss'
+  styleUrl: './blog-categories.component.scss',
 })
 export class BlogCategoriesComponent {
   menu: Observable<List<any>>;
@@ -25,5 +20,34 @@ export class BlogCategoriesComponent {
   constructor(menuService: MenuService, router: Router) {
     this.menu = menuService.getMenu();
     this.router = router;
+  }
+
+  navigateToTag(tag: string): void {
+    if (tag === 'all') {
+      this.router.navigateByUrl('/');
+      return;
+    }
+
+    this.router.navigateByUrl('tag/' + tag);
+  }
+
+  isSelected(tag: string): boolean {
+    const currentTag = this.getCurrentTagFromUrl();
+    return currentTag === tag;
+  }
+
+  private getCurrentTagFromUrl(): string {
+    const url = this.router.url || '/';
+
+    if (url === '/' || url === '') {
+      return 'all';
+    }
+
+    if (url.startsWith('/tag/')) {
+      const rawTag = url.split('/tag/')[1]?.split('?')[0]?.split('#')[0] ?? '';
+      return decodeURIComponent(rawTag || 'all');
+    }
+
+    return 'all';
   }
 }
